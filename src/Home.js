af37f48cc1297f94ae7203fb4d621a161999989a
Bahareh <izad@bu.edu>
2023-05-14 19:53:14 -0400

    chnaged the text on the home page

--- src/Home.js
+++ src/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
     'Regular exercise and workout routines are crucial for maintaining a healthy lifestyle.',
     'It helps improve physical fitness, boost energy levels, and enhance overall well-being.',
     'Our website offers a comprehensive workout planning tool that can help you create a personalized workout plan based on your goals, preferences, and fitness level.',
-    'Whether you\'re looking to build strength, lose weight, or improve endurance, our workout planner can generate a plan tailored to your needs.Start planning your fitness journey today! Click the button below to create your workout plan.'
+    'Whether you\'re looking to build strength, lose weight, or improve endurance, our workout planner can generate a plan tailored to your needs. Start planning your fitness journey today! Click the button below to create your workout plan.'
   ];
 
   useEffect(() => {
